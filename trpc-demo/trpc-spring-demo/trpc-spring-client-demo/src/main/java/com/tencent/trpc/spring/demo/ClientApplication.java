ffadaaa114149d9abaec2e0a71dc5ad64a37c7ff
luckyhhe <luckyhhe@tencent.com>
2024-05-15 14:26:45 +0800

    example of non-compliant code with default settings

--- trpc-demo/trpc-spring-demo/trpc-spring-client-demo/src/main/java/com/tencent/trpc/spring/demo/ClientApplication.java
+++ trpc-demo/trpc-spring-demo/trpc-spring-client-demo/src/main/java/com/tencent/trpc/spring/demo/ClientApplication.java
@@ -25,6 +25,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * ClientApplication.
+ */
 @EnableTRpc
 @SpringBootApplication
 public class ClientApplication {
